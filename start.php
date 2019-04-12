ef3ee06d3e95e7d7fd6d6b92cad33994766ae7b3
Charul Singhvi <charulsinghvi@users.noreply.github.com>
2019-04-12 16:30:24 +0530

    Charul's Commit

--- start.php
+++ start.php
@@ -1,12 +1,4 @@
 <?php
-
-
-include('start.php');
-
-?>
-
-
-<?php
 	include('session.php');
     $sql = 'SELECT * FROM sales';
 	$result = mysqli_query($db,$sql); 
@@ -16,7 +8,6 @@ include('start.php');
     	$rows[] = $row;	
 	}
 	include('inventoryAction.php');
-	
 ?>
 
 <html>
@@ -38,7 +29,7 @@ include('start.php');
 	</head>
 
 	<body>
-	<!---	<div id="top-bar">
+		<div id="top-bar">
 			<nav>
 				<ul>
 					<li><a href="dashboard.php">Home</a></li>
@@ -62,78 +53,8 @@ include('start.php');
 		</div>
 
 		<div id="block">
--->
-		<div id="main-area">
-			<div id="welcome">
-				<h1>Welcome <?php echo $login_session; ?></h1>
-				
-				<br>
-				<!-- /.row -->
-				<div>
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h3 class="card-title">Responsive Hover Table</h3>
-
-<div class="card-tools">
-<div class="input-group input-group-sm" style="width: 150px;">
-<input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-<div class="input-group-append">
-<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-</div>
-</div>
-</div>
-</div>
-<!-- /.card-header -->
-<div class="card-body table-responsive p-0">
-<table class="table table-hover">
-<tr>
-<th>ID</th>
-<th>User</th>
-<th>Date</th>
-<th>Status</th>
-<th>Reason</th>
-</tr>
-<tr>
-<td>183</td>
-<td>John Doe</td>
-<td>11-7-2014</td>
-<td><span class="tag tag-success">Approved</span></td>
-<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-</tr>
-<tr>
-<td>219</td>
-<td>Alexander Pierce</td>
-<td>11-7-2014</td>
-<td><span class="tag tag-warning">Pending</span></td>
-<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-</tr>
-<tr>
-<td>657</td>
-<td>Bob Doe</td>
-<td>11-7-2014</td>
-<td><span class="tag tag-primary">Approved</span></td>
-<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-</tr>
-<tr>
-<td>175</td>
-<td>Mike Doe</td>
-<td>11-7-2014</td>
-<td><span class="tag tag-danger">Denied</span></td>
-<td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-</tr>
-</table>
-</div>
-<!-- /.card-body -->
-</div>
-<!-- /.card -->
-</div>
-</div><!-- /.row -->
-</div>
-			</div>
 		</div>
+
 	</body>
 
 </html>
